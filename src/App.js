692b54251e18a24b2527dab0de6afdafc5d68f99
Asabeneh <asabeneh@gmail.com>
2020-02-20 12:40:38 +0200

    state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ import Main from './components/Main'
 import Footer from './components/Footer'
 
 class App extends Component {
+  state = {
+    count: 0,
+    name: 'Asabeneh',
+    users: ['David', 'Tomas', 'Matias']
+  }
+
+  changeName = () => {
+    let name = this.state.name === 'Asabeneh' ? 'David' : 'Asabeneh'
+    this.setState({
+      name: name
+    })
+  }
+  addOne = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+  minusOne = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
   render() {
     const title = 'Getting Started React'
     const subtitle = 'JavaScript library'
@@ -14,8 +32,15 @@ class App extends Component {
     const date = 'Date: Feb 20, 2020'
     const techs = ['HTML', 'CSS', 'JavaScript']
     const copyRight = new Date().getFullYear()
+
     return (
       <div>
+        <h1>{this.state.count}</h1>
+        <h1>{this.state.name}</h1>
+        <h1>Numbers of users:{this.state.users.length}</h1>
+        <button onClick={this.addOne}>+one</button>
+        <button onClick={this.minusOne}>-one</button>
+        <button onClick={this.changeName}>Change Name</button>
         <Header title={title} subtitle={subtitle} author={author} date={date} />
         <Main techs={techs} />
         <Footer copyRight={copyRight} />
